18fd15f76e07db70a644ab8b8ef7a05438c25129
HyunJun Kim <hyunjun@Hera.local>
2018-10-01 17:51:15 +0900

    Update binary

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 import TextShield from "./TextShield";
 import Encoder from "./Encoder";
 
-/*
-import Decoder from "./Decoder";
-import EncodedMessage from "./EncodedMessage";
-
-let encoder = new Encoder();
-let decoder = new Decoder();
-
-let enc = encoder.encode("010-1234-5678", true).toString();
-
-console.log(enc);
-
-let dec = decoder.decode(EncodedMessage.parse(enc));
-
-console.log(dec);
-*/
-
 (<any>window).TextShieldEncoder = new Encoder();
 
 window.onload = function(): void {
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.ts",
   mode: "production",
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: "ts-loader",
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: "bundle.js",
+    filename: "textshield.js",
     path: path.resolve(__dirname, "dist")
   },
   optimization: {
